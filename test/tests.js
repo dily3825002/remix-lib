862b98353a5a3dc31fbca0549c89cf4b51e7d069
Dily <zhaoxin@egret.com>
2018-12-25 12:18:45 +0800

    init

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+require('./astwalker.js')
+require('./eventManager.js')
+require('./sourceMappingDecoder.js')
+require('./util.js')
+require('./txFormat.js')
+
+require('./traceManager.js')
+require('./codeManager.js')
+require('./disassembler.js')
